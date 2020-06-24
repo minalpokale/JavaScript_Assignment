150ebb3de09ecca6b35f2f376b146c8100750dd1
minalpokale <67097841+minalpokale@users.noreply.github.com>
2020-06-24 12:35:55 +0530

    Add files via upload

---
+++ combinations.js
@@ -0,0 +1,9 @@
+var str="dog"
+var x=str.charAt(0);
+var y=str.substring(0,2);
+var z=str.substring(0,3);
+var a=str.substring(1,2);
+var b=str.substring(1,3);
+var c=str.substring(2,3);
+
+document.writeln(x+ ","+y+","+z+","+a+","+b+","+c);
